2e24ccfc1229a3332ff195351efa6e22ab54a00f
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-02-19 12:33:14 +0000

    export update  🐿 v2.7.0

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,7 +1,29 @@
-import { formatFetchError } from '../index';
+import nError, {
+	formatFetchError,
+	isFetchError,
+	createCustomError,
+} from '../index';
+
+describe('nError', () => {
+	it('exported correctly', () => {
+		expect(typeof nError.notFound).toBe('function');
+	});
+});
 
 describe('formatFetchError', () => {
 	it('exported correctly', () => {
 		expect(typeof formatFetchError).toBe('function');
+	});
+});
+
+describe('isFetchError', () => {
+	it('exported correctly', () => {
+		expect(typeof isFetchError).toBe('function');
+	});
+});
+
+describe('createCustomError', () => {
+	it('exported correctly', () => {
+		expect(typeof createCustomError).toBe('function');
 	});
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import errorCreator from './creator';
+import nError from './creator';
 
+export { createCustomError } from './creator';
 export { formatFetchError } from './formatter';
 export { isFetchError } from './checker';
 export { CATEGORIES as ERROR_CATEGORIES } from './constants';
 
-export default errorCreator;
+export default nError;
